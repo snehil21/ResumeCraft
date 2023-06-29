bb3bd0211d45221bc385bea89f37e6c747636594
snehil <snehil.code09@gmail.com>
2023-06-30 02:51:18 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import react from "react";
+import Header from "./components/Header/Header";
+import Body from "./components/Body/Body";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import resumeSvg from "../../assets/standout.svg";
+
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <p className={styles.heading}>
+          A <span>Resume</span> that stands out!
+        </p>
+        <p className={styles.heading}>
+          Create <span>Impressive </span>Resumes in Minutes.{" "}
+          <span>It's free.</span>
+        </p>
+      </div>
+      <div className={styles.right}>
+        <img src={resumeSvg} alt="Resume" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
